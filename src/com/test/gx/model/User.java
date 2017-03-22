ed9c2c68fb77b68e73e1a9d794e3cc4493b048a4
Allen <867892406@qq.com>
2017-03-22 11:01:59 +0800

    mybatis_generator

---
+++ src/com/test/gx/model/User.java
@@ -0,0 +1,63 @@
+package com.test.gx.model;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+    private String address;
+
+    private String phoneNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber == null ? null : phoneNumber.trim();
+    }
+}
